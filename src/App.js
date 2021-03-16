129d0c21f850e166862956b93403bbb3fb3522b9
sossw1 <sossw1@gmail.com>
2021-03-15 21:11:06 -0400

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function NavBar() {
+  return (
+    <nav>
+      <div className='nav-wrapper blue darken-4'>
+        <title className='brand-logo'>Directory</title>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
