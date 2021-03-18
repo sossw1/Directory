5f3b9a4b6dea3f859cc10fd3eaadf899ef033c0a
sossw1 <sossw1@gmail.com>
2021-03-17 20:52:32 -0400

    App is now stateful, fixed imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-// import './App.css'; 
+// Import React and Component
+import React, {Component} from 'react';
+// Import components
 import NavBar from './components/NavBar/NavBar';
 import Container from './components/Container/Container';
+// Materialize
+import M from 'materialize-css';
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <Container />
-    </>
-  );
+class App extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+    M.AutoInit();
+  }
+
+  render () {
+    return (
+      <>
+        <NavBar />
+        <Container />
+      </>
+    );
+  }
 }
 
 export default App;
