366f74a6c212f5810a33f688de88a9d654939921
sossw1 <sossw1@gmail.com>
2021-03-17 20:36:53 -0400

    Added stateful container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import './App.css'; 
 import NavBar from './components/NavBar/NavBar';
+import Container from './components/Container/Container';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Container />
     </>
   );
 }
---
+++ src/components/Container/Container.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import M from 'materialize-css';
+
+class Container extends Component {
+
+  state = {
+    
+  }
+
+  componentDidMount() {
+    M.AutoInit();
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        
+      </div>
+    )
+  }
+}
+
+export default Container;
