6929cc43fc96aa9d44264020a00409618172b6b9
sossw1 <sossw1@gmail.com>
2021-03-20 16:10:29 -0400

    Dummy button

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Sort() {
   return (
     <div className='container'>
-      
+      <button className='btn-small waves-effect waves-lighten blue darken-4'>Sort by Something</button>
     </div>
   );
 }
