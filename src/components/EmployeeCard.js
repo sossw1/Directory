c826a636a7a5fc31ce58a48a7bbbb3549b45b92e
sossw1 <sossw1@gmail.com>
2021-03-20 16:26:57 -0400

    Added EmployeeCard component

---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EmployeeCard() {
+  return(
+    <>
+      card
+    </>
+  )
+}
+
+export default EmployeeCard;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import EmployeeCard from './EmployeeCard';
 
 function SearchResults() {
   return (
     <div className='container'> 
       {/*map to cards for each employee*/}
+      <EmployeeCard />
     </div>
   );
 }
