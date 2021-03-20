1f2c5d7effc13c2996ae02ab7c419452a9d14094
sossw1 <sossw1@gmail.com>
2021-03-20 19:03:55 -0400

    Get 20 results

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = {
   search: function() {
-    return axios.get('https://randomuser.me/api/');
+    return axios.get('https://randomuser.me/api/?results=20');
   }
 }
 
