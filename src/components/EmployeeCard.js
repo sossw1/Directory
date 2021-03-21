4723e92e4155ad9cf812c530632925dcc4c3480f
sossw1 <sossw1@gmail.com>
2021-03-20 21:40:31 -0400

    Responsiveness

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -4,10 +4,13 @@ function EmployeeCard(props) {
   return(
     <div className="col s12 m6">
       <div className="card blue darken-4">
+        <div className='card-image'>
+          <img src={props.data.picture.large} />
+        </div>
         <div className="card-content white-text">
-          <span className="card-title">Name: {props.data.name.first} {props.data.name.last}</span>
-          <p>Location: {props.data.location.city}, {props.data.location.state}</p>
-          <p>Phone Number: {props.data.phone}</p>
+          <span className="card-title flow-text">Name: {props.data.name.first} {props.data.name.last}</span>
+          <p className='flow-text'>City: {props.data.location.city}, {props.data.location.state}</p>
+          <p className='flow-text'>Phone Number: {props.data.phone}</p>
         </div>
       </div>
     </div>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,12 +3,10 @@ import EmployeeCard from './EmployeeCard';
 
 function SearchResults(props) {
   return (
-    <div className='container'>
-      <div className='row'>
-        {props.employees.map((person, index) => (
-          <EmployeeCard data={person} key={index} />
-        ))}
-      </div>
+    <div className='row'>
+      {props.employees.map((person, index) => (
+        <EmployeeCard data={person} key={index} />
+      ))}
     </div>
   );
 }
